cb59074cb641e6c31d214454bf00b5bd214625f9
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-07-03 05:28:58 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(string)
-array = string.split(" ")
-new_array = []
-array.collect do |word| 
-  new_array << word.reverse
+def reverse_each_word(greeting_string)
+greeting_array = greeting_string.split(" ")
+reversed_words = []
+greeting_array.collect do |word| 
+  reversed_words << word.reverse
   end
-  new_string = new_array.join(" ")
+  greeting_with_reversed_words = reversed_words.join(" ")
 end
