8c2cc1ce791e5898a95842885c35b6d9cceb369e
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-07-12 23:37:17 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(greeting_string)
 reversed_words = []
-greeting_string.split(" ").collect do |word| 
+greeting_string.split(" ").collect do |word|
   reversed_words << word.reverse
-  end
+end
   reversed_words.join(" ")
 end
