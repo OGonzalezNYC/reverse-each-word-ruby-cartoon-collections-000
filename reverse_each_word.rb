bb9b1259547e0e8928f0993785c204c1d909e553
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-07-18 18:54:52 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,14 @@
 def reverse_each_word(greeting_string)
+reversed_words = []
+greeting_string.split(" ").each do |word|
+    reversed_words << word.reverse
+    reversed_words
+  end
+  reversed_words.join(" ")
+end
+
+
+def reverse_each_word(greeting_string)
 reversed_words = greeting_string.split(" ").collect do |word|
   word.reverse
 end
